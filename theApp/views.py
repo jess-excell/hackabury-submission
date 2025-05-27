aebdfd0609c7d89ee54eb0f7e87d4f131353d9df
A-Khandan <akhandan@outlook.com>
2025-05-27 13:02:15 +0100

    first commit

---
+++ theApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.views import View
+from django.http import HttpResponse
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
