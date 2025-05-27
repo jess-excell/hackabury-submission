853f3591dfdb5d840b672fbaf8b067c4541d7f30
jess-excell <jessexcell@outlook.com>
2025-05-27 22:44:10 +0100

    Add passing of information between pages using session variables. This should later be replaced with querysets or something but for the time being all data has placeholders

--- theApp/views.py
+++ theApp/views.py
@@ -11,16 +11,27 @@ class HomeView(View):
 def submit_form(request):
     if request.method == 'POST':
         # save as session variables
+        
+        
         request.session["first_name"] = request.POST.get('first_name')
         request.session["last_name"] = request.POST.get('last_name')
-        request.session[""]
-        # then load new page which can check session variables (i hope)
-        first_name = request.session.get("first_name")
-        print(first_name)
-        print("Last name:", request.POST.get('last_name'))
-        print("From:", request.POST.get('from'))
-        print("To:", request.POST.get('to'))
-        print("Email:", request.POST.get('email'))
+        request.session["from"] = request.POST.get('from')
+        request.session["to"] = request.POST.get('to')
+        request.session["departure"] = request.POST.get('departure')
+        request.session["arrival"] = request.POST.get('arrival')
+        request.session["number_of_travellers"] = request.POST.get('number of travellers')
+        request.session["email"] = request.POST.get('email')
+        
+        request.session["airport_transfer"] = request.POST.get('airport_transfer')
+        request.session["airport_fast_track"] = request.POST.get('airport_fast_track')
+        request.session["airport_lounge"] = request.POST.get('airport_lounge')
+        request.session["taxi"] = request.POST.get('taxi')
+        request.session["insurance"] = request.POST.get('insurance')
+        request.session["travel_money"] = request.POST.get('travel_money')
+        request.session["activities_and_festivals"] = request.POST.get('activities_and_festivals')
+        request.session["parking"] = request.POST.get('parking')
+        
+        
         return redirect("search")
     return HttpResponse("Invalid method", status=405)
 
@@ -31,9 +42,24 @@ class ActivitiesView(View):
 
 class SearchView(View):
     def get(self, request):
-        variables = {
-            "first_name": request.session.get("first_name"),
-            "last_name":  request.session.get("last_name")
-        }
         
-        return render(request, "search.html", variables)
+        params = {
+            "first_name": request.session.get("first_name"),
+            "last_name": request.session.get("last_name"),
+            "from": request.session.get("from"),
+            "to": request.session.get("to"),
+            "departure": request.session.get("departure"),
+            "arrival": request.session.get("arrival"),
+            "number_of_travellers": request.session.get("number_of_travellers"),
+            "email": request.session.get("email"),
+            
+            "airport_transfer": request.session.get("airport_transfer"),
+            "airport_fast_track": request.session.get("airport_fast_track"),
+            "airport_lounge": request.session.get("airport_lounge"),
+            "taxi": request.session.get("taxi"),
+            "insurance": request.session.get("insurance"),
+            "travel_money": request.session.get("travel_money"),
+            "activities_and_festivals": request.session.get("activities_and_festivals"),
+            "parking": request.session.get("parking"),    
+        }        
+        return render(request, "search.html", params)
