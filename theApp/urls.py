71c61fcb72beb737dabf68365a1db912df9654cb
jess-excell <jessexcell@outlook.com>
2025-05-27 20:07:07 +0100

    Add admin interface

--- theApp/urls.py
+++ theApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('admin/', admin.site.urls),
 ]
